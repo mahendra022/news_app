d0c5eff4a1a857bfe0f88ccfeb4d841a562507f1
mahendra022 <ekamahendra86@gmail.com>
2022-05-14 18:02:47 +0700

    create function url_launcher

---
+++ lib/components/url_launcher.dart
@@ -0,0 +1,6 @@
+import 'package:url_launcher/url_launcher.dart';
+
+Future<void> openUrl(String url) async {
+  final Uri _url = Uri.parse(url);
+  await launchUrl(_url, mode: LaunchMode.inAppWebView);
+}
