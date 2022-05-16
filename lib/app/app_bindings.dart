2c974b90925545d858aa49765e1241d82b9d8aa6
mahendra022 <ekamahendra86@gmail.com>
2022-05-16 21:29:03 +0700

    Update app binding

--- lib/app/app_bindings.dart
+++ lib/app/app_bindings.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 
 import 'controllers/news_controller.dart';
+import 'controllers/search_controller.dart';
 
 class AppBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<NewsController>(() => NewsController());
+    Get.lazyPut<SearchController>(() => SearchController());
   }
 }
